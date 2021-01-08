10262536f3d85d657856934eaae4a4266dce551e
olivelegras <olive.leg@gmail.com>
2021-01-08 09:56:22 +0100

    3 files modified

--- src/meteo/meteo.component.ts
+++ src/meteo/meteo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-meteo',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meteo.component.css']
 })
 export class MeteoComponent implements OnInit {
+
+  @Input() title: string;
+
 
   constructor() { }
 
